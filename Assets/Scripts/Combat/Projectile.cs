2bdf4d57a967e23a6963ce201051289ae7b37c84
Vultys <vlad.serdiuk750@gmail.com>
2024-08-27 13:57:46 +0300

    Lecture #10.8. Make an arrow

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace CCC.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1f;
+
+        [SerializeField] private Transform _target = null;
+
+        private void Update()
+        {
+            if (_target == null) return;
+
+            transform.LookAt(GetAimLocation())  ;
+
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = _target.GetComponent<CapsuleCollider>();
+
+            if (targetCapsule == null)
+            {
+                return _target.position;
+            }
+
+            return _target.position + Vector3.up * targetCapsule.height / 2;
+        }
+    }
+}
