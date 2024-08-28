77afedd89bc16c64714e3b686045c3e2a662d43d
Vultys <vlad.serdiuk750@gmail.com>
2024-08-28 19:16:45 +0300

    Lecture #10.21. Pickup Respawning

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,17 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace CCC.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
+        [SerializeField] private float _respawnTime = 5f;
+
         [SerializeField] private Weapon _weapon;
+
+        [SerializeField] private SphereCollider _coliider;
 
         private void OnTriggerEnter(Collider other)
         {
             if(other.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(_weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(_respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            _coliider.enabled = shouldShow;
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
