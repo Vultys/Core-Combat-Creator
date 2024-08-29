2745d8b4efe8338f43c48368d518a4ab426cdb19
Vultys <vlad.serdiuk750@gmail.com>
2024-08-29 21:41:11 +0300

    Lectures #11.1. Progression Design

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCC.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(0, 99)]
+        [SerializeField] private int _startingLevel = 1;
+
+        [SerializeField] private CharacterClass _characterClass;
+    }
+}
