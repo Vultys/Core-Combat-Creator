7dae0ba15cda536bb2b05a467ae88be7124458ba
Vultys <vlad.serdiuk750@gmail.com>
2024-09-04 23:09:15 +0300

    Lecture #12.8. In Game UI

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CCC.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
