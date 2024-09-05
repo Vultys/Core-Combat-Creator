5556ee3d13f1c3bbc32b977816b39c42fd912133
Vultys <vlad.serdiuk750@gmail.com>
2024-09-05 13:02:02 +0300

    Lecture #12.10. Spawning Damage Text

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace CCC.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] private DamageText _damageTextPrefab;
+
+        private void Start()
+        {
+            float damage = 0;
+            Spawn(damage);
+        }
+
+        public void Spawn(float damage)
+        {
+            var instance = Instantiate<DamageText>(_damageTextPrefab, transform);
+        }
+    }
+}
